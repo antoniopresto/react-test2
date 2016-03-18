8c8ee9620d996850969a57ec55cbefe2e0075619
Antonio Presto <antoniopresto@gmail.com>
2016-03-18 10:27:22 -0300

    init

---
+++ src/routes.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import {IndexRoute, Route} from 'react-router';
+import { isLoaded as isAuthLoaded, load as loadAuth } from 'redux/modules/auth';
+import {
+    App,
+    Home,
+    Login,
+    LoginSuccess,
+    NotFound,
+  } from './containers';
+
+export default (store) => {
+  const requireLogin = (nextState, replace, cb) => {
+    function checkAuth() {
+      const { auth: { user }} = store.getState();
+      if (!user) {
+        // oops, not logged in, so can't be here!
+        replace('/');
+      }
+      cb();
+    }
+
+    if (!isAuthLoaded(store.getState())) {
+      store.dispatch(loadAuth()).then(checkAuth);
+    } else {
+      checkAuth();
+    }
+  };
+
+  /**
+   * Please keep routes in alphabetical order
+   */
+  return (
+    <Route path="/" component={App}>
+      <IndexRoute component={Home}/>
+
+      <Route onEnter={requireLogin}>
+        {/* <Route path="chat" component={Chat}/> */}
+        <Route path="loginSuccess" component={LoginSuccess}/>
+      </Route>
+
+      <Route path="login" component={Login}/>
+
+      <Route path="*" component={NotFound} status={404} />
+    </Route>
+  );
+};
